cd42f382c6b7c8762c8710ef27b04629eb8e863c
Rebecca Still <beckystill@hotmail.com>
2019-11-12 21:45:26 +0000

    playground implements ISecurity

--- ThemeParkStartPoint/src/main/java/attractions/Playground.java
+++ ThemeParkStartPoint/src/main/java/attractions/Playground.java
@@ -1,8 +1,17 @@
 package attractions;
 
-public class Playground extends Attraction {
+import people.Visitor;
+
+public class Playground extends Attraction implements behaviours.ISecurity {
 
     public Playground(String name, int rating) {
+
         super(name, rating);
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() > 15) {
+            return false;
+        } return true;
     }
 }
