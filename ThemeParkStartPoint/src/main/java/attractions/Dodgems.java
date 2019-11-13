43c58b29b883485db199bdc5052d399d5b7cf395
Rebecca Still <beckystill@hotmail.com>
2019-11-13 07:11:00 +0000

    Dodgems implements ITicketed

--- ThemeParkStartPoint/src/main/java/attractions/Dodgems.java
+++ ThemeParkStartPoint/src/main/java/attractions/Dodgems.java
@@ -1,8 +1,27 @@
 package attractions;
 
-public class Dodgems extends Attraction {
+import people.Visitor;
+
+public class Dodgems extends Attraction implements behaviours.ITicketed {
+
+    private double defaultPrice;
 
     public Dodgems(String name, int rating) {
+
         super(name, rating);
+        this.defaultPrice = 4.50;
+    }
+
+
+    public double defaultPrice() {
+        return this.defaultPrice;
+    }
+
+    public double priceForVisitor(Visitor visitor) {
+        if (visitor.getAge() < 12) {
+            return defaultPrice()/2;
+        } return defaultPrice();
     }
 }
+
+
